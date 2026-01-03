27c90883cb53bef1285efee724b3f3f198c68b75
Arman11217 <ahrashel1213@gmail.com>
2026-01-03 23:39:06 +0600

    upload

--- obj/Debug/net9.0/PBLC.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/PBLC.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PBLC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de2080971eaa4a988cbc3c06f2cffc12ef5aec7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PBLC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PBLC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
